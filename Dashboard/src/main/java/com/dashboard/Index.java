77c391bcbc9fb0385f02aec1751db2ebd37ff6c1
Fede046 <malserfede@gmail.com>
2024-09-06 20:11:45 +0200

    Update Index.java

--- Dashboard/src/main/java/com/dashboard/Index.java
+++ Dashboard/src/main/java/com/dashboard/Index.java
@@ -1,7 +1,7 @@
 package com.dashboard;
 
 import com.dashboard.Model.HelloApplication;
-
+//vfmsdklfsd
 public class Index {
     public static void main(String[] args) {
         HelloApplication.main(args);
