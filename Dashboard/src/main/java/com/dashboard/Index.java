5eed15dbc54dc8cc50efbafb3740392ea35aa979
Fede046 <malserfede@gmail.com>
2024-09-06 20:15:26 +0200

    Update Index.java

--- Dashboard/src/main/java/com/dashboard/Index.java
+++ Dashboard/src/main/java/com/dashboard/Index.java
@@ -5,5 +5,6 @@ import com.dashboard.Model.HelloApplication;
 public class Index {
     public static void main(String[] args) {
         HelloApplication.main(args);
+        System.out.println("hjsrj");
     }
 }
